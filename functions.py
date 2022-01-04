a3f3825ad3e6be14d449e956e36a96570aac35d1
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-04 15:09:00 +0500

    Added a new print statement from dejawad branch

--- functions.py
+++ functions.py
@@ -5,3 +5,4 @@ print("first modification for second commit")
 print("modification for the sixth real commit")
 
 print("Added this line from the github website for the first time")
+print("added this line from devjawad branch from git terminal")
