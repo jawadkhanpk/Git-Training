1e0c57fdda611c286b64894f166cb50e8609a442
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-04 15:33:18 +0500

    new commit

--- functions.py
+++ functions.py
@@ -7,6 +7,14 @@ print("modification for the sixth real commit")
 print("Added this line from the github website for the first time")
 print("added this line from devjawad branch from git terminal")
 
+<<<<<<< HEAD
 
 #--------------Added a variable
 a = 40
+=======
+#--------------Adding variable
+H = 100
+
+#--------------Adding variable
+U = 500
+>>>>>>> devjawad
